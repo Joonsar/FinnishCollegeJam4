23de1521f4d065138cdcd691d3ceac0ef6685e21
Joon <sarin.joonas@live.fi>
2023-03-27 13:19:45 +0300

    Added EnemyHealth Script

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float hitPoints = 100f;
+
+    // create public method which reduses hitpoints by the amount of damage
+    public void TakeDamage(float damage)
+    {
+        BroadcastMessage("OnDamageTaken");
+        hitPoints -= damage;
+        if (hitPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
