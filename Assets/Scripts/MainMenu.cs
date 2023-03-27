cb6757c2f654493ce3ddc35016bfc4f959ea3c85
Joon <sarin.joonas@live.fi>
2023-03-27 12:54:36 +0300

    Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("Loading Next Screen");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
+}
