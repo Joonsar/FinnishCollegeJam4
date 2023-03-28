6f590a3b4c45172dea0bc13f451c00c9fa8d1ab5
Joon <sarin.joonas@live.fi>
2023-03-28 19:23:30 +0300

    timer added

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+public class Timer : MonoBehaviour
+{
+    public float timeRemaining = 600f; // 10 minutes in seconds
+    public TextMeshProUGUI timerText;
+
+    void Update()
+    {
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+            timerText.text = "Time: " + Mathf.FloorToInt(timeRemaining / 60f).ToString("00") + ":" + Mathf.FloorToInt(timeRemaining % 60f).ToString("00");
+        }
+        else
+        {
+            // End the game
+            SceneManager.LoadScene("MainMenu"); // Need to Build GameOverScene
+        }
+    }
+}
