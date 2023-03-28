402bd548e198254f3aafe34439df6e94b02652a3
xirtoth <henry.heino11@gmail.com>
2023-03-28 13:59:18 +0300

    SkillBar + cooldown napit.

--- Assets/Scripts/Skill.cs
+++ Assets/Scripts/Skill.cs
@@ -1,18 +1,49 @@
+using RPGCharacterAnims.Actions;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Skill : MonoBehaviour
+public class Skill
 {
   
     public string Name { get; set; }
     public string Description { get; set; }
     public int Damage { get; set; }
 
-    public Skill(string name, string description, int damage, int type) 
+    private float currentCooldown;
+
+    private GameObject uiController;
+
+    public float Cooldown { get; set; }
+
+    public int Id { get; set; }
+
+    public Skill(int id, string name, int damage, float cooldown) 
     {
+
+        Id = id;
         Name = name;
-        Description = description;
+        Cooldown = cooldown;
         Damage = damage;
+        currentCooldown = cooldown;
+        uiController = GameObject.FindGameObjectWithTag("UiController");
+    }
+
+    public void UpdateSkill()
+    {
+        currentCooldown -= Time.deltaTime;
+        uiController.GetComponent<UIController>().ChangeSkillCooldown(Id, currentCooldown/Cooldown);
+        if (currentCooldown < 0 )
+        {
+            Attack();
+            currentCooldown = Cooldown;
+           
+            
+        }
+    }
+
+    public virtual void Attack()
+    {
+        
     }
 }
